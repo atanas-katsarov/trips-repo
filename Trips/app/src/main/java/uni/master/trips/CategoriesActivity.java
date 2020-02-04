6fd40841e53e4aee2b5b4b6f4d05faa3cfb0541e
atanas-katsarov <atanaskatsarov1401681073@gmail.com>
2020-02-04 17:09:25 +0200

    Added 2 toolbars when user is logged in and logged out

--- Trips/app/src/main/java/uni/master/trips/CategoriesActivity.java
+++ Trips/app/src/main/java/uni/master/trips/CategoriesActivity.java
@@ -1,11 +1,13 @@
 package uni.master.trips;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class CategoriesActivity extends AppCompatActivity {
 
@@ -15,16 +17,48 @@ public class CategoriesActivity extends AppCompatActivity {
         setContentView(R.layout.categories);
 //
 //        setTitle("My new title");
-
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 //        getSupportActionBar().setTitle("Categories");
     }
 
+//    @Override
+//    public boolean onPrepareOptionsMenu(Menu menu) {
+//        if (true) {
+//            MenuItem mi = (MenuItem) menu.findItem(R.id.profile);
+//            mi.setVisible(true);
+//
+//        } else {
+//            MenuItem mi = (MenuItem) menu.findItem(R.id.login);
+//            mi.setVisible(true);
+//        }
+//        return true;
+//    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.toolbar_menu, menu);
+        if (false) {
+            inflater.inflate(R.menu.toolbar_logged_in, menu);
+        } else {
+            inflater.inflate(R.menu.toolbar_logged_out, menu);
+        }
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.login:
+                return true;
+            case R.id.logout:
+                return true;
+            case R.id.register:
+                return true;
+            case R.id.profile:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
