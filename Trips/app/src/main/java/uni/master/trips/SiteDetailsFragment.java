6b9471d3eb9880495c9e82da22e4c75d944a1dd8
Stoyanov22 <iliyan.stoyanov.22@gmail.com>
2020-02-19 21:52:17 +0200

    Load image from remove API

--- Trips/app/src/main/java/uni/master/trips/SiteDetailsFragment.java
+++ Trips/app/src/main/java/uni/master/trips/SiteDetailsFragment.java
@@ -1,21 +1,30 @@
 package uni.master.trips;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Context;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.github.twocoffeesoneteam.glidetovectoryou.GlideToVectorYou;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
 
 import java.util.Objects;
 
@@ -27,6 +36,10 @@ public class SiteDetailsFragment extends Fragment {
     private Integer idSite;
     private FirebaseFirestore db;
     private OnDetailsInteractionListener mListener;
+    private TextView title;
+    private TextView desc;
+    private TextView country;
+    private ImageView flag;
 
     public SiteDetailsFragment() {
         // Required empty public constructor
@@ -46,28 +59,64 @@ public class SiteDetailsFragment extends Fragment {
         if (getArguments() != null) {
             idSite = getArguments().getInt(ID_SITE);
         }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_site_details, container, false);
+
+        title = view.findViewById(R.id.title_details);
+        desc = view.findViewById(R.id.desc_details);
+        country = view.findViewById(R.id.country_details);
+        flag = view.findViewById(R.id.flag_details);
 
         db = FirebaseFirestore.getInstance();
         db.collection("Sites").whereEqualTo("id", idSite).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
             public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                if(task.isSuccessful()){
+                if (task.isSuccessful()) {
                     for (QueryDocumentSnapshot siteSnapshot : Objects.requireNonNull(task.getResult())) {
-                        Site site = siteSnapshot.toObject(Site.class);
-                        Toast.makeText(getActivity().getApplicationContext(), "Pishki", Toast.LENGTH_LONG).show();
+                        final Site site = siteSnapshot.toObject(Site.class);
+                        @SuppressLint("StaticFieldLeak")
+                        AsyncTask asyncTask = new AsyncTask() {
+                            @Override
+                            protected Object doInBackground(Object[] objects) {
+                                OkHttpClient client = new OkHttpClient();
+                                Request request = new Request.Builder()
+                                        .url("https://restcountries.eu/rest/v2/name/" + site.getCountryName()).build();
+                                Response response = null;
+
+                                try {
+                                    response = client.newCall(request).execute();
+                                    return response.body().string();
+                                } catch (Exception e) {
+                                    Toast.makeText(getActivity().getApplicationContext(), "Couldn't load the country flag", Toast.LENGTH_LONG).show();
+                                }
+
+                                return null;
+                            }
+
+                            @Override
+                            protected void onPostExecute(Object o) {
+                                Activity activity = getActivity();
+                                String url = o.toString();
+                                String flagPath = url.substring(url.lastIndexOf("https"), url.lastIndexOf("\",\"regionalBlocs"));
+                                flag.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+                                GlideToVectorYou.justLoadImage(activity, Uri.parse(flagPath), flag);
+                            }
+                        }.execute();
+                        title.setText(site.getName());
+                        desc.setText(site.getDescription());
+                        country.setText(site.getCountryName());
                     }
                 } else {
                     Toast.makeText(getActivity().getApplicationContext(), "Couldn't load site details", Toast.LENGTH_LONG).show();
                 }
             }
         });
-    }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_site_details, container, false);
+        return view;
     }
 
     public void onButtonPressed(Uri uri) {
